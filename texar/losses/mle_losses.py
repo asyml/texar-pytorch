d29551bf780b316944cd46ae7e9dcc04c5e79865
Pengzhi Gao <pengzhi.gao@petuum.com>
2019-05-01 13:04:14 -0400

    update mle_losses

--- texar/losses/mle_losses.py
+++ texar/losses/mle_losses.py
@@ -201,7 +201,79 @@ def sequence_sparse_softmax_cross_entropy(labels,
     return losses
 
 
+def sequence_sigmoid_cross_entropy(labels,
+                                   logits,
+                                   sequence_length,
+                                   average_across_batch=True,
+                                   average_across_timesteps=False,
+                                   average_across_classes=True,
+                                   sum_over_batch=False,
+                                   sum_over_timesteps=True,
+                                   sum_over_classes=False,
+                                   time_major=False,
+                                   stop_gradient_to_label=False,
+                                   name=None):
+    """Computes sigmoid cross entropy for each time step of sequence
+    predictions.
 
+    Args:
+        labels: Target class distributions.
+
+            - If :attr:`time_major` is `False` (default), this must be a\
+            Tensor of shape `[batch_size, max_time(, num_classes)]`.
+
+            - If `time_major` is `True`, this must be a Tensor of shape\
+            `[max_time, batch_size(, num_classes)]`.
+
+            Each row of `labels` should be a valid probability
+            distribution, otherwise, the computation of the gradient will be
+            incorrect.
+        logits: Unscaled log probabilities having the same shape as with
+            :attr:`labels`.
+        sequence_length: A Tensor of shape `[batch_size]`. Time steps beyond
+            the respective sequence lengths will have zero losses.
+        average_across_timesteps (bool): If set, average the loss across
+            the time dimension. Must not set `average_across_timesteps`
+            and `sum_over_timesteps` at the same time.
+        average_across_batch (bool): If set, average the loss across the
+            batch dimension. Must not set `average_across_batch`'
+            and `sum_over_batch` at the same time.
+        average_across_classes (bool): If set, average the loss across the
+            class dimension (if exists). Must not set
+            `average_across_classes`' and `sum_over_classes` at
+            the same time. Ignored if :attr:`logits` is a 2D Tensor.
+        sum_over_timesteps (bool): If set, sum the loss across the
+            time dimension. Must not set `average_across_timesteps`
+            and `sum_over_timesteps` at the same time.
+        sum_over_batch (bool): If set, sum the loss across the
+            batch dimension. Must not set `average_across_batch`
+            and `sum_over_batch` at the same time.
+        sum_over_classes (bool): If set, sum the loss across the
+            class dimension. Must not set `average_across_classes`
+            and `sum_over_classes` at the same time. Ignored if
+            :attr:`logits` is a 2D Tensor.
+        time_major (bool): The shape format of the inputs. If `True`,
+            :attr:`labels` and :attr:`logits` must have shape
+            `[max_time, batch_size, ...]`. If `False`
+            (default), they must have shape `[batch_size, max_time, ...]`.
+        stop_gradient_to_label (bool): If set, gradient propagation to
+            :attr:`labels` will be disabled.
+        name (str, optional): A name for the operation.
+
+    Returns:
+        A Tensor containing the loss, of rank 0, 1, or 2 depending on the
+        arguments
+        :attr:`{average_across}/{sum_over}_{timesteps}/{batch}/{classes}`.
+        For example, if the class dimension does not exist, and
+
+        - If :attr:`sum_over_timesteps` and :attr:`average_across_batch`  \
+        are `True` (default), the return Tensor is of rank 0.
+
+        - If :attr:`average_across_batch` is `True` and other arguments are \
+        `False`, the return Tensor is of shape `[max_time]`.
+    """
+    if stop_gradient_to_label:
+        labels = labels.detach()
 
 
 
