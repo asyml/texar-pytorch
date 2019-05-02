8c59dab79a71853efc96de9fde21a3e268655209
“Avinash” <avinash.bukkittu@petuum.com>
2019-05-02 16:59:49 -0400

    Added test cases for ConvNetwork1D

--- texar/modules/networks/conv_networks_test.py
+++ texar/modules/networks/conv_networks_test.py
@@ -28,36 +28,39 @@ class Conv1DNetworkTest(unittest.TestCase):
         outputs_1 = network_1(inputs_1)
         self.assertEqual(outputs_1.shape, torch.Size([128, 256]))
 
-        """hparams = {
+        hparams = {
             # Conv layers
             "num_conv_layers": 2,
-            "filters": 128,
+            "in_channels": [64, 128],
+            "out_channels": 128,
             "kernel_size": [[3, 4, 5], 4],
-            "other_conv_kwargs": {"padding": "same"},
+            "other_conv_kwargs": {"padding": 0},
             # Pooling layers
-            "pooling": "AveragePooling",
+            "pooling": "AvgPool1d",
             "pool_size": 2,
-            "pool_strides": 1,
+            "pool_stride": 1,
             # Dense layers
             "num_dense_layers": 3,
-            "dense_size": [128, 128, 10],
-            "dense_activation": "relu",
-            "other_dense_kwargs": {"use_bias": False},
+            "in_features": [113152, 128, 128],
+            "out_features": [128, 128, 10],
+            "dense_activation": "ReLU",
+            "other_dense_kwargs": None,
             # Dropout
             "dropout_conv": [0, 1, 2],
             "dropout_dense": 2
         }
         network_2 = Conv1DNetwork(hparams)
-        # nlayers = nconv-pool + nconv + npool + ndense + ndropout + flatten
+        # dropout-merge-dropout-conv-avgpool-dropout-flatten-(Sequential(Linear,ReLU))-(Sequential(
+        # Linear,ReLU))-dropout-linear
         self.assertEqual(len(network_2.layers), 1+1+1+3+4+1)
-        self.assertTrue(isinstance(network_2.layer_by_name("conv_pool_1"),
+        self.assertTrue(isinstance(network_2.layer_by_name("MergeLayer"),
                                    tx.core.MergeLayer))
         for layer in network_2.layers[1].layers:
-            self.assertTrue(isinstance(layer, tx.core.SequentialLayer))
+            self.assertTrue(isinstance(layer, nn.Sequential))
 
-        inputs_2 = tf.ones([64, 16, 300], tf.float32)
+        inputs_2 = torch.ones([128, 64, 300])
         outputs_2 = network_2(inputs_2)
-        self.assertEqual(outputs_2.shape, [64, 10])"""
+        self.assertEqual(outputs_2.shape, torch.Size([128, 10]))
 
 
 if __name__ == "__main__":
