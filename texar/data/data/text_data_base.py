18607d30b1d45364e5d4be3b6b20dad53e67147e
Zecong Hu <huzecong@gmail.com>
2019-06-05 17:19:44 -0400

    Fix zlib compression in TextLineDataSource

--- texar/data/data/text_data_base.py
+++ texar/data/data/text_data_base.py
@@ -15,8 +15,9 @@
 Base text data class that is inherited by all text data classes.
 """
 import io
+import locale
 from abc import ABC
-from typing import IO, Iterator, Optional, TypeVar
+from typing import IO, Iterator, Optional, TypeVar, AnyStr, BinaryIO
 
 import torch
 from texar.data.data.data_base import DataBase, DataSource
@@ -40,15 +41,18 @@ class TextLineDataSource(DataSource[str]):
 
     def __init__(self, file_paths: MaybeList[str],
                  compression_type: Optional[str] = None,
+                 encoding: Optional[str] = None,
                  delimiter: Optional[str] = None,
                  max_length: Optional[int] = None):
         r"""Construct a :class:`TextLineDataSource` instance.
 
         Args:
             file_paths (str or list[str]): Paths to the text files.
-            compression_type (str): The compression type for the text files,
-                ``"gzip"`` and ``"zlib"`` are supported. Default is ``None``,
-                in which case files are treated as plain text files.
+            compression_type (str, optional): The compression type for the text
+                files, ``"gzip"`` and ``"zlib"`` are supported. Default is
+                ``None``, in which case files are treated as plain text files.
+            encoding (str, optional): Encoding for the files. By default uses
+                the default locale of the system (usually UTF-8).
             delimiter (str, optional): Delimiter for tokenization purposes. This
                 is used in combination with ``max_length``.
             max_length (int, optional): Maximum length for data examples. Length
@@ -68,6 +72,7 @@ class TextLineDataSource(DataSource[str]):
         if isinstance(file_paths, str):
             file_paths = [file_paths]
         self._compression_type = compression_type
+        self._encoding = encoding or locale.getpreferredencoding()
         self._file_paths = file_paths
         self._max_length = max_length
         self._delimiter = delimiter
@@ -75,37 +80,72 @@ class TextLineDataSource(DataSource[str]):
             raise ValueError("'max_length' and 'delimiter' should both be"
                              "None or not None")
 
-    class _ZlibWrapper(IO[bytes]):
-        def __init__(self, file: io.BufferedIOBase):
+    class _ZlibWrapper(io.BufferedReader):
+        def __init__(self, raw: IO[bytes]):
+            super().__init__(raw)  # type: ignore
             import zlib
-            self.file = file
+            self.file = raw
             self.zlib = zlib.decompressobj()
             self.buffer = b''
 
-        def read(self, n) -> bytes:
-            if len(self.buffer) > 0:
-                b = self.buffer[:n]
-                self.buffer = self.buffer[n:]
-                return b
-            while True:
-                b = self.zlib.decompress(self.file.read(n))
-                if len(b) > 0:
-                    break
-            if len(b) > n:
-                self.buffer = b[n:]
-                return b[:n]
+        @property
+        def closed(self) -> bool:
+            return self.file.closed
+
+        def readable(self) -> bool:
+            return True
+
+        def close(self) -> None:
+            self.file.close()
+
+        def read1(self, n: int = -1) -> bytes:
+            # Our implementation does not really satisfy the definition for
+            # `read1`, but whatever, it seems to work.
+            if n == -1:
+                raw = self.file.read(n)
+                b = self.buffer
+                self.buffer = b''
+                if raw:
+                    cur = self.zlib.decompress(raw)
+                    if len(cur) > 0:
+                        if len(b) > 0:
+                            b = b + cur
+                        else:
+                            b = cur
+            else:
+                if len(self.buffer) > 0:
+                    b = self.buffer[:n]
+                    self.buffer = self.buffer[n:]
+                    return b
+                while True:
+                    raw = self.file.read(n)
+                    if not raw:
+                        return b''
+                    b = self.zlib.decompress(raw)
+                    if len(b) > 0:
+                        break
+                if len(b) > n:
+                    self.buffer = b[n:]
+                    return b[:n]
+            return b
+
+        def read(self, n: Optional[int] = -1) -> bytes:
+            if n is None or n < 0:
+                n = -1
+            return self.read1(n)
 
         def __getattr__(self, item):
             return getattr(self.file, item)
 
     def _open_file(self, path: str) -> IO[str]:
         if self._compression_type == 'zlib':
-            f = io.TextIOWrapper(self._ZlibWrapper(path))
+            f: IO[str] = io.TextIOWrapper(  # type: ignore
+                self._ZlibWrapper(open(path, 'rb')), encoding=self._encoding)
         elif self._compression_type == 'gzip':
             import gzip
-            f = gzip.open(path, 'rt')
+            f = gzip.open(path, 'rt', encoding=self._encoding)
         else:
-            f = open(path, 'r')
+            f = open(path, 'r', encoding=self._encoding)
         return f
 
     def __iter__(self) -> Iterator[str]:
