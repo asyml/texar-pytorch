fd25188bfd090a559881b0342698f2782a26fb67
Zecong Hu <huzecong@gmail.com>
2019-06-26 12:54:56 -0400

    Add __len__ to DataIterator

--- texar/data/data/data_base.py
+++ texar/data/data/data_base.py
@@ -608,13 +608,8 @@ class DataBase(Dataset, Generic[RawExample, Example], ABC):
 
     def __len__(self) -> int:
         if self._dataset_size is None:
-            warnings.warn(
-                "The provided data source does not support random access. To "
-                "obtain dataset size, a full traversal must be performed. "
-                "This is often unnecessary and slow, consider redesigning your "
-                "use case.")
-            self._prefetch_all_source()
-            assert self._dataset_size is not None
+            raise TypeError(
+                "__len__ not supported for datasets with undetermined size")
         return self._dataset_size
 
     def process(self, raw_example: RawExample) -> Example:
