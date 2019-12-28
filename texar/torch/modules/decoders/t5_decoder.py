b58325353c6af1fdd186dd25e625581e65d58111
Swapnil Singhavi <cd_svsanya@yahoo.co.in>
2019-12-28 00:05:53 -0500

    fix linting

--- texar/torch/modules/decoders/t5_decoder.py
+++ texar/torch/modules/decoders/t5_decoder.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, Optional, Union
+from typing import Optional, Union
 
 import torch
 from torch import nn
