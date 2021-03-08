9288dfaa7bd2320ec606a784e5364088d2d42bf0
Yishan Jiao <yishan.jiao@petuum.com>
2021-03-08 14:17:16 -0800

    type ignore

--- texar/torch/data/tokenizers/bert_tokenizer.py
+++ texar/torch/data/tokenizers/bert_tokenizer.py
@@ -166,9 +166,9 @@ class BERTTokenizer(PretrainedBERTMixin, TokenizerBase):
         self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab,
                                                       unk_token=self.unk_token)
 
-    def _map_text_to_token(self, text: str) -> \
-        Union[List[str], List[Tuple[str, int, int]]]:  
-        split_tokens = []
+    def _map_text_to_token( # type: ignore
+        self, text: str) -> List[str]:
+        split_tokens: List[str] = []
         if self.do_basic_tokenize:
             for token in self.basic_tokenizer.tokenize(
                     text, never_split=self.all_special_tokens):
